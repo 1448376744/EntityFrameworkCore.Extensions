eb009348a746ea278e15efa204c71b37972c0811
jianfeng <1448376744@qq.com>
2021-11-28 20:14:55 +0800

    添加扩展项

--- src/EntityFrameworkCore.Extensions.SqlServer/Extensions/EntityFrameworkDbContextExtensions.cs
+++ src/EntityFrameworkCore.Extensions.SqlServer/Extensions/EntityFrameworkDbContextExtensions.cs
@@ -22,6 +22,48 @@ namespace Microsoft.EntityFrameworkCore
             var provider = new DapperQueryProvider(context.Database);
             return new DapperQueryableImpl<T>(provider, model);
         }
+   
+        public static IDapperQueryable<T1, T2> Join<T1, T2>(this IDapperQueryable<T1> queryable, Expression<Func<T1, T2, bool>> expression)
+          where T1 : class
+          where T2 : class
+        {
+            var query = queryable as DapperQueryableImpl<T1>;
+            if (query == null)
+            {
+                throw new NotSupportedException();
+            }
+            var multiple = new DapperQueryableImpl<T1, T2>(query.Provider, query.Context.Model);
+            multiple.Join(expression);
+            return multiple;
+        }
+
+        public static IDapperQueryable<T1, T2> LeftJoin<T1, T2>(this IDapperQueryable<T1> queryable, Expression<Func<T1, T2, bool>> expression)
+         where T1 : class
+         where T2 : class
+        {
+            var query = queryable as DapperQueryableImpl<T1>;
+            if (query == null)
+            {
+                throw new NotSupportedException();
+            }
+            var multiple = new DapperQueryableImpl<T1, T2>(query.Provider, query.Context.Model);
+            multiple.LeftJoin(expression);
+            return multiple;
+        }
+
+        public static IDapperQueryable<T1, T2> RightJoin<T1, T2>(this IDapperQueryable<T1> queryable, Expression<Func<T1, T2, bool>> expression)
+        where T1 : class
+        where T2 : class
+        {
+            var query = queryable as DapperQueryableImpl<T1>;
+            if (query == null)
+            {
+                throw new NotSupportedException();
+            }
+            var multiple = new DapperQueryableImpl<T1, T2>(query.Provider, query.Context.Model);
+            multiple.RightJoin(expression);
+            return multiple;
+        }
 
         public static IDapperQueryable<T1, T2> Join<T1, T2>(this IDapperQueryable<T1> queryable, DbSet<T2> table1, Expression<Func<T1, T2, bool>> expression)
            where T1 : class
@@ -34,6 +76,34 @@ namespace Microsoft.EntityFrameworkCore
             }
             var multiple = new DapperQueryableImpl<T1, T2>(query.Provider, query.Context.Model);
             multiple.Join(expression);
+            return multiple;
+        }
+
+        public static IDapperQueryable<T1, T2> LeftJoin<T1, T2>(this IDapperQueryable<T1> queryable, DbSet<T2> table1, Expression<Func<T1, T2, bool>> expression)
+          where T1 : class
+          where T2 : class
+        {
+            var query = queryable as DapperQueryableImpl<T1>;
+            if (query == null)
+            {
+                throw new NotSupportedException();
+            }
+            var multiple = new DapperQueryableImpl<T1, T2>(query.Provider, query.Context.Model);
+            multiple.LeftJoin(expression);
+            return multiple;
+        }
+
+        public static IDapperQueryable<T1, T2> RightJoin<T1, T2>(this IDapperQueryable<T1> queryable, DbSet<T2> table1, Expression<Func<T1, T2, bool>> expression)
+         where T1 : class
+         where T2 : class
+        {
+            var query = queryable as DapperQueryableImpl<T1>;
+            if (query == null)
+            {
+                throw new NotSupportedException();
+            }
+            var multiple = new DapperQueryableImpl<T1, T2>(query.Provider, query.Context.Model);
+            multiple.RightJoin(expression);
             return multiple;
         }
     }
